cda0cde6d7c6ec2a2d3c6774f24d0907888c9485
MiguelSilvaP <miguelsilvadev@hotmail.com>
2021-08-24 23:34:56 -0500

    Almost every test case created, I just need to complete the "GetAllTodos".

---
+++ todomiguel.Test/Tests/TodoApiTest.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using todomiguel.Common.Models;
+using todomiguel.Functions.Entities;
+using todomiguel.Functions.Functions;
+using todomiguel.Test.Helpers;
+using Xunit;
+
+namespace todomiguel.Test.Tests
+{
+    public class TodoApiTest
+    {
+        private readonly ILogger logger = TestFactory.CreateLogger();
+
+        [Fact]
+        public async void CreateTodo_Should_Return_200()
+        {
+            //Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Todo todoRequest = TestFactory.GetTodoRequest();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(todoRequest);
+
+            //Act
+            IActionResult response = await TodoApi.CreateTodo(request, mockTodos, logger);
+
+            //Assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        }
+
+        [Fact]
+        public async void UpdateTodo_Should_Return_200()
+        {
+            //Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Todo todoRequest = TestFactory.GetTodoRequest();
+            Guid todoId = Guid.NewGuid();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(todoId, todoRequest);
+
+            //Act
+            IActionResult response = await TodoApi.UpdateTodo(request, mockTodos, todoId.ToString(), logger);
+
+            //Assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        }
+
+        //Practice with the other three tests
+
+        [Fact]
+        public async void DeleteTodo_Should_Return_200()
+        {
+            //Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Todo todoRequest = TestFactory.GetTodoRequest();
+            TodoEntity todoEntity = TestFactory.GetTodoEntity();
+            Guid todoId = Guid.NewGuid();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(todoId, todoRequest);
+
+            //Act
+            IActionResult response = await TodoApi.DeleteTodo(request, todoEntity, mockTodos, todoId.ToString(), logger);
+
+            //Assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        }
+
+        [Fact]
+        public void GetTodoById_Should_Return_200()
+        {
+            //Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            TodoEntity todoEntity = TestFactory.GetTodoEntity();
+            Guid todoId = Guid.NewGuid();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(todoId);
+
+            //Act
+            IActionResult response = TodoApi.GetTodoById(request, todoEntity, todoId.ToString(), logger);
+
+            //Assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        }
+
+        /*[Fact]
+        public async void GetAllTodos_Should_Return_200()
+        {
+            //Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Todo todoRequest = TestFactory.GetTodoRequest();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(todoRequest);
+
+            //Act
+            IActionResult response = await TodoApi.GetAllTodos(request, mockTodos, logger);
+
+            //Assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        }*/
+    }
+}

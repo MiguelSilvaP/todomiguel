887a4e372b34d414acab74a8139653a578c2df55
MiguelSilvaP <miguelsilvadev@hotmail.com>
2021-08-24 20:29:32 -0500

    Project architecture.

---
+++ todomiguel.Common/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todomiguel.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; } 
+    }
+}
